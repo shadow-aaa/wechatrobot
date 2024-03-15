2bd7f5ffd165c6ee528932732163a063dee4222c
shadow-aaa <1813003722@qq.com>
2024-03-15 12:46:34 +0800

    签到成功自动停止机器人，重复的码不进行签到

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
 from wcferry import Wcf
 from wcferry import wxmsg
 from queue import Empty
-def judge(str1:str):
-    if str1.isdigit() and len(str1)==4:
-        print("接收到签到码，正在发送")
-        robot.send_text(str1,"43806374575@chatroom")
-        robot.send_text(str1,"duifene")
-        return True
+def judge(newcode:str):
+    global oldcode
+    if newcode.isdigit() and len(newcode)==4:
+        if oldcode=="":#第一次接收到签到码
+            oldcode=newcode
+            print("接收到签到码，正在发送")
+            robot.send_text(newcode,"43806374575@chatroom")
+            robot.send_text(newcode,"duifene")
+        elif oldcode==newcode:#群里的人再次发送签到码，且签到码是假的，导致没有退出机器人
+            print("与前一个签到码一致")
+            print("不进行签到")
+            pass
+        else:#新码
+            print("接收到签到码，正在发送")
+            robot.send_text(newcode,"43806374575@chatroom")
+            robot.send_text(newcode,"duifene")
     else:
         print("等待签到码中")
-        return False
+
 robot=Wcf(debug=False,block=True)
 msg=wxmsg
+oldcode=""
 robot.enable_receiving_msg()
 print("开始接收信息")
 while robot.is_receiving_msg():
@@ -20,7 +31,7 @@ while robot.is_receiving_msg():
         if msg.from_self():
             pass
         elif msg.sender=="gh_b05aec8ffdbd":
-            if msg.content=="签到成功":
+            if msg.content.find("签到成功"):
                 break
         else:
             judge(msg.content)
