24bd1c0da4b32edc562434402f502ba962f889a9
shadow-aaa <1813003722@qq.com>
2024-03-18 15:40:03 +0800

    1

--- test.py
+++ test.py
@@ -12,7 +12,6 @@ def judge(newcode:str):
         elif oldcode==newcode:#群里的人再次发送签到码，且签到码是假的，导致没有退出机器人
             print("与前一个签到码一致")
             print("不进行签到")
-            pass
         else:#新码
             print("接收到签到码，正在发送")
             robot.send_text(newcode,"43806374575@chatroom")
@@ -31,7 +30,7 @@ while robot.is_receiving_msg():
         if msg.from_self():
             pass
         elif msg.sender=="gh_b05aec8ffdbd":
-            if msg.content.find("签到成功"):
+            if msg.content=="签到成功！":
                 break
         else:
             judge(msg.content)
